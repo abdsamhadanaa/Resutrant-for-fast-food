bd2aeedf1172ee4261513b035ac8107ac6eea7a7
Abdualrhman S.AbuSamhadana <abedsamhadana@gmail.com>
2020-05-20 22:55:11 +0300

    before the final verstion

--- source.js
+++ source.js
@@ -1,12 +1,9 @@
 var allData=[];
-var math=[];
+var hold=[];
 var count=0;
 var sarech=document.getElementById('se');
 var print=document.getElementById('print').addEventListener("click",print);
-sarech.addEventListener("click",getElment);
-function getElment (){
-
-}
+sarech.addEventListener("keyup",getElment);
 var listBbtn =document.querySelectorAll('.button').forEach(item => {
   item.addEventListener('click', event => {
   	var theMainDiv=event.target.parentElement.children;
@@ -23,16 +20,12 @@ var listBbtn =document.querySelectorAll('.button').forEach(item => {
 
 })
 function print(){
-
-	
-	console.log(allData.length);
-	
+console.log(allData.length);//to cheack the data enter exist and visible .
 var x=allData.length;
 while(x>count){
-		
 		var mainD=document.getElementById("Em");
-
-		var di =document.createElement("div");
+		var di =document.createElement("div");// mainFather 
+		di.className="subfather";
 		mainD.appendChild(di);
 		var meal=document.createElement("h2");
 		var price =document.createElement("p");
@@ -40,13 +33,63 @@ while(x>count){
 			di.appendChild(price);
 			meal.innerHTML=allData[count].m;
 			price.innerHTML=allData[count].p;
+			hold.push(allData[count].m);
+			hold.push(allData[count].p);
+
+
+			var d=document.createElement("INPUT");
+			di.appendChild(d);
+			d.type="checkbox"
+			d.className="dItem"
+			d.value="X";
 			++count;
-			
-	}
-	console.log(count);
+			console.log(d);
+			}
+		}
+/*
 
+document.querySelectorAll(".subfather").forEach(item => {
+	console.log(item);
+	
+  item.addEventListener('change', event => {
+  	if(event.target.checked){
+  		console.log("Dalia")
+  	}
+  	else{
+  		console.log("dsa")
+  	}
+  	
+  	if(event.target.checked==="true"){
+  	console.log(event.target.checked==="true");
+  	var mNd=event.target.parentElement.children;
+  	console.log(mNd);
+  	var c=0;
+  	while(mNd.length>c){
+  		event.target.parentElement.removeChild(mNd[c]);
+  		console.log(event.target.parentElement);
+  		++c;
+  		}}
+  		console.log("hhhh");
+
+  	
+
+})
+
+})
+*/
+
+var cBtn=document.getElementById("cl");
+cBtn.addEventListener("click",clear);
+function clear(){
+document.querySelectorAll('.subfather').forEach(item =>{
+item.parentNode.removeChild(item);	
+})
 }
-
-
-
-
+function getElment(e){
+	var data=e.target.value;
+	var result=hold.filter((x)=>{
+		return x===data;
+	});
+	alert(result.join("")
+		);
+}
